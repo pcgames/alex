3d6c7eee5187883e25f7c3ac44d2e2f7f43bb1ae
Almaslovsckij <almaslovsckij@gmail.com>
2016-08-14 20:01:12 +0300

    добавили горизонтальные вертикальные классы

--- snake/Program.cs
+++ snake/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace snake
 {
     class Program
@@ -11,14 +12,14 @@ namespace snake
 
         static void Main(string[] args)
         {
-            Point p1 = new Point(5,8,'&');
+          /*  Point p1 = new Point(5,8,'&');
             p1.Draw();
             Point p2 = new Point(6, 7, '*');
             p2.Draw();
             List<int> l1=new List<int>();
-            l1.Add( 0);
+            l1.Add( 17);
             l1.Add( 1);
-            l1.Add( 2);
+            l1.Add( 99);
             foreach (int i in l1)
             {
                 Console.WriteLine(i);
@@ -28,9 +29,12 @@ namespace snake
             l1.Clear();
             List<Point> tp = new List<Point>();
             tp.Add(p1);
-            tp.Add(p2);
-
-                Console.ReadLine();
+            tp.Add(p2);*/
+            Horizontal_lines r = new Horizontal_lines(1,10,25);
+            Vertical_lines g = new Vertical_lines(1, 32, 14);
+            g.Draw();
+            r.Draw();
+            Console.ReadLine();
         }
     }
 }
